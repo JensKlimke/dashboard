18c3253b20a302034d8cdb5482de363ea00154de
Jens Klimke <jens.klimke@rwth-aachen.de>
2020-12-16 13:46:49 +0100

    Added some content

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          My personal <code>dashboard</code> project in the blank version.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/JensKlimke/dashboard"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Get code
         </a>
       </header>
     </div>
